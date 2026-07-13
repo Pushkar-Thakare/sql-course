69c7ebfc511a3ef4b353e330e259afc771c9bd07
Pushkar-Thakare <pushkarthakare30@gmail.com>
2026-07-13 08:13:58 +0530

    using WHERE 2

--- sql/basics/01_select_film.sql
+++ sql/basics/01_select_film.sql
@@ -1,7 +1,8 @@
 -- Using WHERE
 
-SELECT *
+-- get title of all R rated movies whose replacement cost is above $25
+
+SELECT title
 FROM film
-WHERE replacement_cost > 25 ;
-
-
+WHERE replacement_cost > 25 
+AND rating ='R'
