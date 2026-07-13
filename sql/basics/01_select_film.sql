f9d4de668bb18d766b0e72a820411aa605254de6
Pushkar-Thakare <pushkarthakare30@gmail.com>
2026-07-13 12:19:58 +0530

    changes

--- sql/basics/01_select_film.sql
+++ sql/basics/01_select_film.sql
@@ -3,6 +3,9 @@
 -- get title of all R rated movies whose replacement cost is above $25
 
 SELECT title
+-- get title of all R rated movies whose replacement cost is above $25
+
+SELECT title
 FROM film
 WHERE replacement_cost > 25 
 <<<<<<< Updated upstream
